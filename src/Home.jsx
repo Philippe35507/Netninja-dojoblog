e006c3576b63109cb25d7ee8bf131492dc0ec6b2
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-10 15:33:00 +0100

    onclick event: the event parameter

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,14 +2,16 @@ const Home = () => {
   const handleClick = () => {
     console.log("Hello");
   };
-  const handleClickAgain = (name) => {
-    console.log("Hello " + name);
+  const handleClickAgain = (name, e) => {
+    console.log("Hello " + name, e.target);
   };
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={() => handleClickAgain("Mario")}>Click me again</button>
+      <button onClick={(e) => handleClickAgain("Mario", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
