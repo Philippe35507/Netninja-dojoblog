4ed883fda43c148477057b12e88c5066d30e7fca
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-10 15:57:10 +0100

    Outputting Lists

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,44 +1,27 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("Mario");
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName("Luigi");
-    setAge(30);
-    console.log(name);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old.
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default Home;
-
-/*
-const Home = () => {
-  let name = "Mario";
-  const handleClick = () => {
-    name = "Luigi";
-    console.log(name);
-  };
-
-  return (
-    <div className="home">
-      <h2>Homepage</h2>
-      <p>{name}</p>
-      <button onClick={handleClick}>Click me</button>
-    </div>
-  );
-};
-
-export default Home;
-*/
