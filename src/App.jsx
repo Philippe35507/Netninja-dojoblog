71e9321fac3f927ddb1e821e4d0251ee36022658
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-10 15:10:39 +0100

    Adding styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="content"></div>
-      <Home />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
