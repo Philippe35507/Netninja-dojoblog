bd9d0ccb7c176c5ba7e18feff9482a722d241b00
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-10 15:28:13 +0100

    onClick event: passing an argument into the function

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,15 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("Hello");
+  };
+  const handleClickAgain = (name) => {
+    console.log("Hello " + name);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain("Mario")}>Click me again</button>
     </div>
   );
 };
