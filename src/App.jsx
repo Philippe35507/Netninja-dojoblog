eb19a94869cf219b2ae91737732174464ef21170
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-10 14:57:51 +0100

    Multiple Components
    <Navbar />  <Home />

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import "./index.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "Welcome to our new blog";
-  const likes = 50;
-  const link = "http://www.google.com";
-  // Dynamic values: strings(and used as attributes like href), numbers, arrays. No boleans, no objects.
-
   return (
     <div className="App">
-      <div>
-        <h1 className="content">App Component</h1>
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <a href={link}>Google website</a>
-      </div>
+      <Navbar />
+      <div className="content"></div>
+      <Home />
     </div>
   );
 }
