df2559095c40ff7d0f52a9fe979978f14d7be417
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-11 18:17:22 +0100

    Run useEffect whenever a certain value changes (and only this value)
    useEffect dependencies

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,6 +13,8 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("Mario");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
@@ -20,12 +22,14 @@ const Home = () => {
 
   useEffect(() => {
     console.log("useEffect ran");
-    console.log(blogs);
-  }, []);
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName("Luigi")}>Change name</button>
+      <p>{name} </p>
     </div>
   );
 };
