829ac60414e9b10c4f5504d72b54333f81b17f8b
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-22 11:46:18 +0100

    Deleting blogs 2/2 redirect to Homepage
    useNavigate() Hook

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -8,9 +8,12 @@ const BlogDetails = () => {
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+  const navigate = useNavigate();
 
   const handleClick = () => {
-    fetch("http://localhost:8000/blogs/" + blog.id, { method: "DELETE" });
+    fetch("http://localhost:8000/blogs/" + blog.id, { method: "DELETE" }).then(
+      () => navigate("/")
+    );
   };
 
   return (
